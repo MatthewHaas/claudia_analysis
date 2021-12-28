887450708a3b25ca6b5b1fd913e4b30cf66248f2
Matthew Haas <matthew.haas1@gmail.com>
2021-12-28 12:14:01 -0600

    Fix typos

--- filter_vcfs/concat_filtered_vcfs.sh
+++ filter_vcfs/concat_filtered_vcfs.sh
@@ -11,26 +11,26 @@
 #SBATCH -e concat_filtered_vcfs.err
 
 # Include path to the working directory
-cd /scratch.global/haasx092/claudia_analysis/211170_snp_calling_results
+cd /scratch.global/haasx092/claudia_analysis/211227_snp_calling_results
 
 module load bcftools
 
-chr1='211127_snp_calling_results_ZPchr0001_filtered.recode.vcf'
-chr2='211127_snp_calling_results_ZPchr0002_filtered.recode.vcf'
-chr3='211127_snp_calling_results_ZPchr0003_filtered.recode.vcf'
-chr4='211127_snp_calling_results_ZPchr0004_filtered.recode.vcf'
-chr5='211127_snp_calling_results_ZPchr0005_filtered.recode.vcf'
-chr6='211127_snp_calling_results_ZPchr0006_filtered.recode.vcf'
-chr7='211127_snp_calling_results_ZPchr0007_filtered.recode.vcf'
-chr8='211127_snp_calling_results_ZPchr0008_filtered.recode.vcf'
-chr9='211127_snp_calling_results_ZPchr0009_filtered.recode.vcf'
-chr10='211127_snp_calling_results_ZPchr0010_filtered.recode.vcf'
-chr11='211127_snp_calling_results_ZPchr0011_filtered.recode.vcf'
-chr12='211127_snp_calling_results_ZPchr0012_filtered.recode.vcf'
-chr13='211127_snp_calling_results_ZPchr0013_filtered.recode.vcf'
-chr14='211127_snp_calling_results_ZPchr0014_filtered.recode.vcf'
-chr15='211127_snp_calling_results_ZPchr0015_filtered.recode.vcf'
-scf16='211127_snp_calling_results_ZPchr0016_filtered.recode.vcf'
-scf458='211127_snp_calling_results_ZPchr0458_filtered.recode.vcf'
+chr1='211227_snp_calling_results_ZPchr0001_filtered.recode.vcf'
+chr2='211227_snp_calling_results_ZPchr0002_filtered.recode.vcf'
+chr3='211227_snp_calling_results_ZPchr0003_filtered.recode.vcf'
+chr4='211227_snp_calling_results_ZPchr0004_filtered.recode.vcf'
+chr5='211227_snp_calling_results_ZPchr0005_filtered.recode.vcf'
+chr6='211227_snp_calling_results_ZPchr0006_filtered.recode.vcf'
+chr7='211227_snp_calling_results_ZPchr0007_filtered.recode.vcf'
+chr8='211227_snp_calling_results_ZPchr0008_filtered.recode.vcf'
+chr9='211227_snp_calling_results_ZPchr0009_filtered.recode.vcf'
+chr10='211227_snp_calling_results_ZPchr0010_filtered.recode.vcf'
+chr11='211227_snp_calling_results_ZPchr0011_filtered.recode.vcf'
+chr12='211227_snp_calling_results_ZPchr0012_filtered.recode.vcf'
+chr13='211227_snp_calling_results_ZPchr0013_filtered.recode.vcf'
+chr14='211227_snp_calling_results_ZPchr0014_filtered.recode.vcf'
+chr15='211227_snp_calling_results_ZPchr0015_filtered.recode.vcf'
+scf16='211227_snp_calling_results_ZPchr0016_filtered.recode.vcf'
+scf458='211227_snp_calling_results_ZPchr0458_filtered.recode.vcf'
 
 bcftools concat $chr1 $chr2 $chr3 $chr4 $chr5 $chr6 $chr7 $chr8 $chr9 $chr10 $chr11 $chr12 $chr13 $chr14 $chr15 $scf16 $scf458 > merged_vcf_files.vcf
